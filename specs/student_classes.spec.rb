d23e83d4ffa508a6d9d0c8c895435d730e25ef05
Callum <callumpb@hotmail.com>
2017-10-02 16:09:44 +0100

    commit setup 2

--- specs/student_classes.spec.rb
+++ specs/student_classes.spec.rb
@@ -3,3 +3,9 @@ require('minitest/rg')
 require_relative('../student_classes.rb')
 
 class TestStudent < MiniTest::Test
+
+@student
+
+def setup
+  @student = Student.new("Callum", E16)
+end
---
+++ student_classes.rb
@@ -0,0 +1,6 @@
+class Student
+
+  def initialize(input_name, input_cohort)
+    @name = input_name
+    @cohort = input_cohort
+  end
